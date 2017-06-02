5901597f49c9d436ec3752d8ff1c5c23d89ef972
Feng Qiu <q@pagerendered.in>
2017-06-02 22:30:20 +0800

    git subrepo pull server/gj-schema
    subrepo:
      subdir:   "server/gj-schema"
      merged:   "1eed03e"
    upstream:
      origin:   "git@bitbucket.org:kehang/gj-schema.git"
      branch:   "master"
      commit:   "1eed03e"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

--- server/gj-schema/models/testItems.js
+++
@@ -1,9 +0,0 @@
-const TestItems = new Mongo.Collection('testItems');
-
-TestItems.deny({
-  insert() { return true; },
-  update() { return true; },
-  remove() { return true; },
-});
-
-export default TestItems;
