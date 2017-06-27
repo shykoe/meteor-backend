650fc983bcb1b815c614b0b5c02dbe0d4835fc7a
Feng Qiu <q@pagerendered.in>
2017-06-27 11:34:40 +0800

    删除原有的schema目录

--- server/gj-schema/models/categories.js
+++
@@ -1,9 +0,0 @@
-const Categories = new Mongo.Collection('categories');
-
-Categories.deny({
-  insert() { return true; },
-  update() { return true; },
-  remove() { return true; },
-});
-
-export default Categories;
--- server/gj-schema/models/feedbacks.js
+++
@@ -1,9 +0,0 @@
-const Feedbacks = new Mongo.Collection('feedbacks');
-
-Feedbacks.deny({
-  insert() { return true; },
-  update() { return true; },
-  remove() { return true; },
-});
-
-export default Feedbacks;
