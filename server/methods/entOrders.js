5ab4182d6de1f2d2927d0e305346f0c5948f6aca
Feng Qiu <q@pagerendered.in>
2017-07-18 10:27:54 +0800

    后端安全加强和获取订单列表方法改进

--- server/methods/entOrders.js
+++ server/methods/entOrders.js
@@ -1,43 +1,124 @@
+import Consts from '/server/pr-schema/consts';
 import Orders from '/server/pr-schema/models/orders';
 import Reports from '/server/pr-schema/models/reports';
 import Users from '/server/pr-schema/models/users';
 import Categories from '/server/pr-schema/models/categories';
 
+function enhanceOrders(orders) {
+  // Fetch the agent usernames and tester usernames referenced in these orders
+  // as we need them for display.
+  const userDict = {}
+  for (const order of orders) {
+    if (order.agentId) {
+      userDict[order.agentId] = 1;
+    }
+    if (order.testerIds) {
+      for (const testerId of order.testerIds) {
+        userDict[testerId] = 1;
+      }
+    }
+  }
+
+  const users = Users.find({
+    _id: { $in: Object.keys(userDict) }
+  }, {
+    fields: { _id: 1, username: 1 }
+  }).fetch();
+  for (const user of users) {
+    userDict[user._id] = user.username;
+  }
+
+  for (const order of orders) {
+    if (order.agentId) {
+      order.agent = userDict[order.agentId];
+    }
+    if (order.testerIds) {
+      const testers = [];
+      for (const testerId of order.testerIds) {
+        testers.push(userDict[testerId]);
+      }
+      order.testers = testers;
+    }
+  }
+
+  return orders;
+}
+
 Meteor.methods({
   //find own orders
   'agent.myorders.get': (userName, role, page, perpage, field, order) => {
-  	const userid = Meteor.call('user.getid',userName);
-  	const skiped = ( parseInt(page) - 1 ) * parseInt(perpage);
-  	const orderid = order == 'ASC' ? 'asc':'desc';
-    return Orders.find({'agent':userid},{skip:parseInt(skiped), limit:parseInt(perpage), sort:[[ field, orderid ]] }).fetch();
+    // 首先确保当前用户已经登录并且是企业员工
+    const currentUser = Meteor.user();
+    if (!currentUser) { return { errors: '用户未登录' }; }
+    if (!(currentUser.role < Consts.USER_ROLE_NORMAL)) { return { errors: '用户权限不足' }; }
+
+  	const skipped = (parseInt(page) - 1) * parseInt(perpage);
+  	const sortOrder = order == 'ASC' ? 'asc' : 'desc';
+    const orders = Orders.find({ agentId: currentUser._id }, {
+      skip: parseInt(skipped),
+      limit: parseInt(perpage),
+      sort: [[ field, sortOrder ]]
+    }).fetch();
+
+    return enhanceOrders(orders);
   },
-  'agent.orders.get':(userName, role, page, perpage, field, order, filter) => {
-    const skiped = ( parseInt(page) - 1 ) * parseInt(perpage);
-    const orderid = order == 'ASC' ? 'asc':'desc';
-    const nowFilter = filter? filter:{};
-    return Orders.find(nowFilter,{skip:parseInt(skiped), limit:parseInt(perpage), sort:[[ field, orderid ]] }).fetch();
+
+  'agent.orders.get': (userName, role, page, perpage, field, order, filter) => {
+    // 首先确保当前用户已经登录并且是企业员工
+    const currentUser = Meteor.user();
+    if (!currentUser) { return { errors: '用户未登录' }; }
+    if (!(currentUser.role < Consts.USER_ROLE_NORMAL)) { return { errors: '用户权限不足' }; }
+
+    const skipped = (parseInt(page) - 1) * parseInt(perpage);
+    const sortOrder = order == 'ASC' ? 'asc' : 'desc';
+    const orders = Orders.find(filter || {}, {
+      skip: parseInt(skipped),
+      limit: parseInt(perpage),
+      sort: [[ field, sortOrder ]]
+    }).fetch();
+
+    return enhanceOrders(orders);
   },
+
   //find one order by id
-  'order.get':(id) =>{
-    return Orders.findOne({'_id':id});
+  'order.get': (id) => {
+    // 首先确保当前用户已经登录并且是企业员工
+    const currentUser = Meteor.user();
+    if (!currentUser) { return { errors: '用户未登录' }; }
+    if (!(currentUser.role < Consts.USER_ROLE_NORMAL)) { return { errors: '用户权限不足' }; }
+
+    return enhanceOrders([Orders.findOne({ '_id': id })])[0];
   },
+
   //accept the order, set the order.agent equals id
-  'agent.order.accept':(id, userName) =>{
-    const userId = Users.findOne({username:userName})._id;
-    const status = Orders.findOne({_id:id}).status;
-    var count;
-    //中途agent可以取消领取..所以可能不改变status
-    if(status == 1){
-      count = Orders.update({_id:id}, {$set:{agent:userId, status:2}});
-      return Promise.resolve('ok');
-    }else{
-      count = Orders.update({_id:id}, {$set:{agent:userId}});
-      return Promise.resolve('ok');
-    }
+  'agent.order.accept': (id, userName) => {
+    // 首先确保当前用户已经登录并且是企业员工
+    const currentUser = Meteor.user();
+    if (!currentUser) { return { errors: '用户未登录' }; }
+    if (!(currentUser.role < Consts.USER_ROLE_NORMAL)) { return { errors: '用户权限不足' }; }
+
+    const result = Orders.update({ _id: id }, {
+      $set: {
+        agentId: currentUser._id
+      }
+    });
+    return Promise.resolve('ok');
   },
-  'agent.order.unpick':(id)=>{
-    const count = Orders.update({_id:id}, {$set:{agent:null}});
+
+  'agent.order.unpick': (id) => {
+    // 首先确保当前用户已经登录并且是企业员工
+    const currentUser = Meteor.user();
+    if (!currentUser) { return { errors: '用户未登录' }; }
+    if (!(currentUser.role < Consts.USER_ROLE_NORMAL)) { return { errors: '用户权限不足' }; }
+
+    const result = Orders.update({ _id: id }, {
+      $set: {
+        agentId: undefined
+      }
+    });
+    return Promise.resolve('ok');
   },
+
   //agent approved the order
   'agent.order.approve':(id, data)=>{
     const { categoryName, levelName, items, agentMsg, status, price, ShippingInfo } = data;
@@ -46,15 +127,16 @@ Meteor.methods({
       return Promise.reject('data error');
     }
     return Orders.findOne({'_id':id});
-
   },
+
   //find the orders by the tester's username
   'tester.orders.get':(userName, role, page, perpage, field, order) =>{
     const userid = Meteor.call('user.getid',userName);
-    const skiped = ( parseInt(page) - 1 ) * parseInt(perpage);
-    const orderid = order == 'ASC' ? 'asc':'desc';
-    return Orders.find({'tester':userid},{skip:parseInt(skiped), limit:parseInt(perpage), sort:[[ field, orderid ]] }).fetch();
+    const skipped = ( parseInt(page) - 1 ) * parseInt(perpage);
+    const sortOrder = order == 'ASC' ? 'asc':'desc';
+    return Orders.find({'tester':userid},{skip:parseInt(skipped), limit:parseInt(perpage), sort:[[ field, sortOrder ]] }).fetch();
   },
+
   //tester update
   'tester.order.update':(id, data, ops, userName)=>{
     const { items } = data;
@@ -65,8 +147,8 @@ Meteor.methods({
       return Promise.reject('data error');
     }
     return Orders.findOne({'_id':id});
-
   },
+
   'tester.img.update':(id, data)=>{
     const rel = Orders.update({'_id':id},{$push:{testingImages:data}});
     if(rel == 0){
@@ -74,6 +156,7 @@ Meteor.methods({
     }
     return Promise.resolve();
   },
+
   'keeper.img.update':(id, data)=>{
     const rel = Orders.update({'_id':id},{$push:{sampleImages:data}});
     if(rel == 0){
@@ -81,37 +164,40 @@ Meteor.methods({
     }
     return Promise.resolve();
   },
+
   // find all orders
   'orders.get':(page, perpage, field, order, filter)=>{
-    const skiped = ( parseInt(page) - 1 ) * parseInt(perpage);
-    const orderid = order == 'ASC' ? 'asc':'desc';
+    const skipped = ( parseInt(page) - 1 ) * parseInt(perpage);
+    const sortOrder = order == 'ASC' ? 'asc':'desc';
     if(filter.status){
-      return Orders.find({status:{$in:filter.status}},{skip:parseInt(skiped), limit:parseInt(perpage), sort:[[ field, orderid ]] }).fetch();
+      return Orders.find({status:{$in:filter.status}},{skip:parseInt(skipped), limit:parseInt(perpage), sort:[[ field, orderid ]] }).fetch();
     }
-    return Orders.find({},{skip:parseInt(skiped), limit:parseInt(perpage), sort:[[ field, orderid ]] }).fetch();
+    return Orders.find({},{skip:parseInt(skipped), limit:parseInt(perpage), sort:[[ field, sortOrder ]] }).fetch();
   },
+
   'keeper.order.update':(id,data)=>{
     const { status } = data;
     const rel = Orders.update({'_id':id},{$set:{status:status}});
     return Orders.findOne({'_id':id});
   },
+
   //assigner set the order's testers
   'assigner.tester.set':(id, data)=>{
     const { tester } = data;
     const rel = Orders.update({'_id':id}, {$set:{tester:tester, status:9}} );
     return Orders.findOne({'_id':id});
   },
+
   'agent.allorder.get': ( page, perpage, field, order) => {
-    const skiped = ( parseInt(page) - 1 ) * parseInt(perpage);
+    const skipped = ( parseInt(page) - 1 ) * parseInt(perpage);
 
-    x=Orders.find({},{skip:parseInt(skiped), limit:parseInt(perpage), sort:[order]}).fetch();
+    x=Orders.find({},{skip:parseInt(skipped), limit:parseInt(perpage), sort:[order]}).fetch();
     return x;
-
   },
-  'agent.allorder.getFilter': ( page, perpage, field, order,myfilter) => {
-    const skiped = ( parseInt(page) - 1 ) * parseInt(perpage);
-    x=Orders.find(myfilter,{skip:parseInt(skiped), limit:parseInt(perpage), sort:[order]}).fetch();
-    return x;
 
+  'agent.allorder.getFilter': ( page, perpage, field, order,myfilter) => {
+    const skipped = ( parseInt(page) - 1 ) * parseInt(perpage);
+    x=Orders.find(myfilter,{skip:parseInt(skipped), limit:parseInt(perpage), sort:[order]}).fetch();
+    return x;
   }
 });
